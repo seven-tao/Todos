116d58c83ce4974d33aa369e89bb71efc085dc42
Seven <seven.liu@gaoenergy.com>
2018-09-28 15:59:45 +0800

    增加删除确认

--- pages/logs/logs.js
+++ pages/logs/logs.js
@@ -5,15 +5,25 @@ Page({
   },
   onShow: function () {
     var logs = wx.getStorageSync('todo_logs')
-    console.log(logs.length);
+    // console.log(logs.length);
     if (logs) {
       this.setData({ logs: logs.reverse() })
     }
   },
   clearLogs:function(){
-    wx.removeStorageSync('todo_logs');
-    console.log(wx.getStorageSync('todo_logs').length);
-    this.setData({ logs: [] });
-    app.data.logs=[];
+    var that=this;
+    wx.showModal({
+      title: '提示',
+      content: '是否清除日志',
+      success:function(res){
+        if(res.confirm)
+        {
+          wx.removeStorageSync('todo_logs');
+          // console.log(wx.getStorageSync('todo_logs').length);
+          that.setData({ logs: [] });
+          app.data.logs = [];
+        }11
+      }
+    });
   }
 })
